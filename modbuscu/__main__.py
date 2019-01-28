51917523d63960dcad9fd199c84cb13f1b65e06b
Bartek <bartlomiej.hirsz@infotech.pl>
2019-01-28 22:13:00 +0100

    Added serial port param from cli

--- modbuscu/__main__.py
+++ modbuscu/__main__.py
@@ -1,21 +1,26 @@
-PORT = 'COM11'
 import logging
 from modbuscu.rekuperator import Rekuperator
 from modbuscu.zasilacz_awaryjny import ZasilaczAwaryjny
 from modbus_tk import modbus_rtu
 import serial
+import sys
 
 if __name__ == "__main__":
+
+    if len(sys.argv)>1:
+        port = sys.argv[1]
+    else:
+        port = 'COM11'
 
     logging.basicConfig(filename='error.log',
                         filemode='a',
                         format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                         level=logging.DEBUG)
     try:
-        server = modbus_rtu.RtuServer(serial.Serial(PORT))
+        server = modbus_rtu.RtuServer(serial.Serial(port))
     except Exception as e:
         print(e)
-        logging.error(f'Wybrany port szeregowy {PORT} nie jest dostępny', exc_info=False)
+        logging.error(f'Wybrany port szeregowy {port} nie jest dostępny', exc_info=False)
         raise ValueError('Wybrany port szeregowy nie jest dostępny')
     server.start()
     rekuperator = Rekuperator(server=server)
